3f83b980a5a9f1d2f2a3309539ffaff323f8f79c
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-08 19:43:52 +0530

    Hellow World

--- Hello.py
+++ Hello.py
@@ -1,3 +1,11 @@
 var1="Manivel"
 var2="Kingn"
-print(len(var1,var2))
+for i in var1:
+    pass
+for j in var2:
+    pass
+# print(i,j,sep="\n")
+if i==j:
+    print(dict(zip(var2,var1)))
+else:
+    print(dict(zip(var1,var2)))
