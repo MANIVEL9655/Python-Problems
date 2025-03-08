72fc7c6bcb85f8642b86cf50e2421e6d73f1f941
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-08 20:02:11 +0530

    Hellow World

---
+++ Array(Smallest).py
@@ -0,0 +1,1 @@
+print("Manivel MRM ")
--- Hello.py
+++ Hello.py
@@ -1,11 +1,15 @@
-var1="Manivel"
-var2="Kingn"
-for i in var1:
-    pass
-for j in var2:
-    pass
-# print(i,j,sep="\n")
-if i==j:
-    print(dict(zip(var2,var1)))
-else:
-    print(dict(zip(var1,var2)))
+def mani(var1,var2):
+    # var1="Manivel"
+    # var2="Kingn"
+    for i in var1:
+        pass
+    for j in var2:
+        pass
+    if i==j:
+        d=(dict(zip(var2,var1)))
+        print(d)
+    else:
+        ds=(dict(zip(var1,var2)))
+        print(ds)
+
+mani("manivel","manivel")
