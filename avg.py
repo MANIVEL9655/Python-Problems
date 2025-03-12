f11f5846bf8537433a1f73140acc1e3f96a45053
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-12 10:50:59 +0530

    Average

--- avg.py
+++ avg.py
@@ -1,12 +1,24 @@
-def average(n):
+# def average(n):
 
-    a=[]
-    for i in range(0,n):
-        ele=int(input("enter value"))
-        a.append(ele)
-    avg=sum(a)/n
-    print(avg)
-n=int(input("how many numbers you want to find the average"))  
-average(n)
+#     a=[]
+#     for i in range(0,n):
+#         ele=int(input("enter value"))
+#         a.append(ele)
+#     avg=sum(a)/n
+#     print(avg)
+# n=int(input("how many numbers you want to find the average"))  
+# average(n)
 
-    
+# def swap(a,b):
+    # temp=a
+    # a=b
+    # b=a
+    # print(a,b)
+# a=int(input("enter"))
+# b=int(input("enter"))
+# swap(a,b)
+a,b=5,3
+temp=a
+a=b
+b=temp
+print(a,b)
