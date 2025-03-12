4654ed23caf1381b26a4a3ca435ffefef43dfd44
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-12 09:49:30 +0530

    pos or neg

---
+++ pos or neg.py
@@ -0,0 +1,9 @@
+def posorneg(a):
+    if a>=0:
+        print("positive")
+    elif a<0:
+        print("Negative")
+    # else:
+    #     print("Invalid")
+a=int(input("Enter the number"))
+print(posorneg(a))
