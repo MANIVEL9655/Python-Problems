a11b77c8bd5c6b9e702b65417b90310c79a55069
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-12 10:00:33 +0530

    Vowel

---
+++ vowel.py
@@ -0,0 +1,7 @@
+def vowel(cha):
+    if cha=='a' or cha=='e' or cha=='i' or cha=='o' or cha=='u':
+        print("its an vowel")
+    else:
+        print("sorry its not an vowel")
+cha=input("enter")
+vowel(cha)
