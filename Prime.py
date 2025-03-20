86bd5c095c0cdf76ef64c7796b4a111c67647919
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-20 21:23:28 +0530

    map()

--- Prime.py
+++ Prime.py
@@ -1,7 +1,3 @@
-def is_prime(n):
-    if n//n==0 and n//1==0:
-        print("The number is prime")
-    else:
-        print("The number is not a prime")
-n=int(input("enter a number"))
-is_prime(n)        
+a=int(input())
+c=str(map(int,input().split()))
+print(hash(c))
--- fact.py
+++ fact.py
@@ -1,9 +1,2 @@
-def fact(n):
-    factorial=1
-    for i in range(1,n+1):
-        factorial=factorial*i
-        print(factorial)
-    else:
-        pass
-n=int(input("jbj"))
-print(fact(n))
+a=int(input().split())
+print(a)
