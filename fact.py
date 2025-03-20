223ded040231a030426b011751abbcdc41b508a1
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-20 21:51:32 +0530

    map()

--- fact.py
+++ fact.py
@@ -1,2 +1,9 @@
-a=int(input().split())
-print(a)
+def fact(n):
+    factorial=1
+    for i in range(1,n+1):
+        factorial=factorial*i
+        print(factorial)
+    else:
+        pass
+n=int(input("jbj"))
+print(fact(n)
---
+++ map().py
@@ -0,0 +1,3 @@
+a=[1,23,4,5,6,71,12]
+c=list(map(str,a.split()))
+print(c)
