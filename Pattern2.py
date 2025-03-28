17179c660af2535c73c5e42657421989eeb06000
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-28 22:14:03 +0530

    The code is created

--- Pattern2.py
+++ Pattern2.py
@@ -1,6 +1,12 @@
 n=5
 for i in range(n):
     for j in range(n):
-        if j<=n-i-1:
-            print(j,end=" ")
+        if j<n-i-1:
+            print(end="  ")
+    for j in range(n):
+        if j<2*i+1:
+            print("*")
+    for j in range(n):
+        if j<n-i-1:
+            print(end=" ")
     print()
