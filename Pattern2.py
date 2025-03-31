63966ccc91414d97703134b0b4534d4c1e3d0ff4
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-03-31 21:53:05 +0530

    Pattern added

--- Pattern2.py
+++ Pattern2.py
@@ -1,12 +1,8 @@
-n=5
-for i in range(n):
-    for j in range(n):
-        if j<n-i-1:
-            print(end="  ")
-    for j in range(n):
-        if j<2*i+1:
-            print("*")
-    for j in range(n):
-        if j<n-i-1:
-            print(end=" ")
-    print()
+def function(n):
+
+    for i in range(1,n):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print()
+n=int(input("Enter the number"))
+function(n)
