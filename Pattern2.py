5c1dde31892013e06029953fe1e8cebff75fd529
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-04-01 21:58:04 +0530

    Pattern added

--- Pattern2.py
+++ Pattern2.py
@@ -1,7 +1,12 @@
 def M_pattern(n):
     for row in range(n):
         for col in range(n):
-            if col==0 or
-            col==n-1 or
-            row == col and row<n/2
-
+            if ((col==0 or
+            col==n-1) or
+            row + col == (n - 1 and row < n / 2) or
+            row == col and row<n/2 ):
+                print("*", end=" ")
+            else:
+                print(" ")
+n=int(input("Enter a Number"))
+M_pattern(n)
