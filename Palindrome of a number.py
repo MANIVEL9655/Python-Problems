c06b491e12536a0327600cb76894bd196c5f9f90
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-04-04 09:55:05 +0530

    Palindrome

---
+++ Palindrome of a number.py
@@ -0,0 +1,11 @@
+
+def palindrom(n):
+    interger_to_string = str(n)
+    reverse_Str= interger_to_string[::-1]
+    print(reverse_Str)
+    if n==reverse_Str:
+        print("The give number is palindrome")
+    else:
+        print("Not a Palindrome")
+n=int(input("Enter the Number"))
+palindrom(n)
