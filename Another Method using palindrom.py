56228da4d4cc36fde26c0b2b5a5270cada36b8d1
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-04-04 11:06:35 +0530

    Palindrome is another added

--- Another Method using palindrom.py
+++ Another Method using palindrom.py
@@ -3,17 +3,15 @@ from numpy.ma.core import count
 
 def pal(n):
     revnum=0
-    count = 0
+    dup = n
     while(n>0):
         last_digit=n%10
         revnum=revnum*10+last_digit
         n=n//10
-        count=count+1
-        print("The count of a number is",count)
-        # if revnum == n:
-        #     print("The Number is Palindrome")
-        # else:
-        #     print("Not a palindrome")
+    if dup==revnum:
+        print("The Number is Palindrome")
+    else:
+        print("Not a palindrome")
     return revnum
 n=int(input("enter a number"))
 print(pal(n))
