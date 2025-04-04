a7340017906f648a5c67c9a50ae490f4d5060659
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-04-04 14:15:37 +0530

    Print All division

--- Prime or Nor.py
+++ Prime or Nor.py
@@ -1,5 +1,13 @@
-n=int(input("Enter the number"))
-if n//1==0 and n//n==0:
-    print("The nummber is prime")
-else:
-    print("Not a prime"
+def prime(n):
+    count=0
+    for i in range(1,n):
+        if(n%i==0):
+            count=count+1
+            if(n/i !=i):
+                count=count+1
+    if count==2:
+        print("True")
+    else:
+        print("False")
+n=int(input("Enter the Number"))
+print(prime(n))
